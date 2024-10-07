d465f39aeaf837af9bac0a8ccff0634607e60f8e
Khon Irina <khon.iv@dns-shop.ru>
2024-10-07 13:10:05 +1000

    Add DisplayProducts.cs and DisplayServices.cs

---
+++ EShop/Commands/DisplayProducts.cs
@@ -0,0 +1,27 @@
+﻿using Core;
+
+namespace EShop.Commands;
+
+public static class DisplayProducts
+{
+    public const string Name = "DisplayProducts";
+    public const string Description = "показать товары";
+    
+    public static void Execute(int productCountForDisplay = -1)
+    {
+        if ((productCountForDisplay < 0 ) || (productCountForDisplay > ProductCatalog.ProductsForDisplay.Count))
+            productCountForDisplay = ProductCatalog.ProductsForDisplay.Count;
+        for (var i = 0; i < productCountForDisplay; i++)
+        {
+            Console.WriteLine(
+                ProductCatalog.ProductsForDisplay[i].Name + "\n" +
+                "Цена: " + ProductCatalog.ProductsForDisplay[i].Price + "\n" +
+                "Остаток: " + ProductCatalog.ProductsForDisplay[i].Remains
+            );
+            if (ProductCatalog.ProductsForDisplay[i].Description != String.Empty)
+                Console.WriteLine("Описание: " + ProductCatalog.ProductsForDisplay[i].Description);
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ EShop/Commands/DisplayServices.cs
@@ -0,0 +1,25 @@
+﻿using Core;
+
+namespace EShop.Commands;
+
+public static class DisplayServices
+{
+    public const string Name = "DisplayServices";
+    public const string Description = "показать услуги";
+    
+    public static void Execute(int serviceCountForDisplay = -1)
+    {
+        if ((serviceCountForDisplay < 0) || (serviceCountForDisplay > ServiceCatalog.ServicesForDisplay.Count))
+            serviceCountForDisplay = ServiceCatalog.ServicesForDisplay.Count;
+        for (var i = 0; i < serviceCountForDisplay; i++)
+        {
+            Console.WriteLine(
+                ServiceCatalog.ServicesForDisplay[i].Name + "\n" +
+                "Цена: " + ServiceCatalog.ServicesForDisplay[i].Price
+            );
+            if (ServiceCatalog.ServicesForDisplay[i].Description != String.Empty)
+                Console.WriteLine("Описание: " + ServiceCatalog.ServicesForDisplay[i].Description);
+            Console.WriteLine();
+        }
+    }
+}
