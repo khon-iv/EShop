e45e84f718b7c79e6baa41bbaa6d1033448cc1a6
Khon Irina <khon.iv@dns-shop.ru>
2024-10-07 12:56:49 +1000

    Add Service class

---
+++ Core/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace Core;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string  Name { get; set; }
+    public Categories Category { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public int Remains { get; set; }
+    
+    public Product(int id, string name, Categories category, int price = 0, int remains = 1, string description = "")
+    {
+        Id = id;
+        Name = name;
+        Category = category;
+        Price = price;
+        Remains = remains;
+        Description = description;
+    }
+
+    public enum Categories
+    {
+        ForHome,
+        ForBeautyAndHealth,
+        Phone,
+        TV,
+        PC
+    }
+}
